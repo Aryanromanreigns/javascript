930569170716614f873246b63543171ebb90a728
Aryan Kumar <aryanempire168@gmail.com>
2026-02-01 00:56:43 +0530

    Add files via upload

---
+++ Basics/03_conversationOperation/server.js
@@ -0,0 +1,46 @@
+let num = "55";
+console.log(typeof num);
+
+
+let changeInNum = Number(num);
+console.log(typeof changeInNum);
+console.log(changeInNum);
+
+/*
+"55"-->55
+"55abc"-->NaN
+true-->1
+*/
+
+/*
+like Number
+same we have Boolean 
+and String
+*/
+
+//-------------------OPERATIONS ----------------------
+let vali = 55;
+let newvali = -vali;
+console.log(newvali);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(6/2);
+console.log(5%2);
+
+let one = "hello";
+let two = "world";
+let three = one + two;
+console.log(three);
+
+console.log(1+"2"); // "12"
+console.log("1"+2); // "12"
+console.log(1+2+"2"); // "32"
+console.log("1"+2+2); // "122"
+
+
+
+
+
+
