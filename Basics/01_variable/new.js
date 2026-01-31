1c404b5bd2e0606fbaed394b383e7ba4ced4c77f
Aryan Kumar <aryanempire168@gmail.com>
2026-02-01 00:24:51 +0530

    Add files via upload

---
+++ Basics/01_variable/new.js
@@ -0,0 +1,11 @@
+const accId = 12345;
+let userName = "aryankumar";
+var accountpass= "securePass123";
+acccity = "New York";
+let accState;
+
+console.table([accId, userName, accountpass, acccity, accState]);
+/*
+if we do not need to change the value then use the const keywoed 
+and for declaring the variable name use the let keyword because var is will change all the value in the global scope.
+*/
---
+++ Basics/02_dataTypes/script.js
@@ -0,0 +1,20 @@
+let name = "aryankumar";
+let roll = 23;
+let isAdmin = false;
+
+
+
+/*
+number 2 to power 53
+bigint
+string = ""
+boolean = true/false
+null
+undefined
+sybol - unique identifier
+object = { key: value }
+
+*/
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
